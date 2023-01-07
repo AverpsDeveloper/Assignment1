2c6ab0eecb23dd7467bba6b91a065aa1664e1d0d
AverpsDeveloper <vanessa@averps.com>
2023-01-07 10:26:16 +0000

    add cluster layer and childcard data

--- script.js
+++ script.js
@@ -23,5 +23,62 @@ async function main() {
         })
     }
 }
+async function loadGeojsonData() {
+    let response = await axios.get("childcare.geojson");
+    console.log(response.data)
+    let childCareCentre = L.geoJson(response.data, {
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup(feature.properties.Description);
+      }
+    }
+    ).addTo(map);
+  }
+  let map = initMap();
+  document.querySelector("#search-btn").addEventListener("click", function() {
+
+    const searchTerms = document.querySelector("#search-terms").value;
+    alert(searchTerms)
+    //       const center = map.getBounds().getCenter();
+    //       const ll = center.lat + "," + center.lng;
+    //       const results = await loadData(searchTerms, ll, 2000);
+  });
+  loadGeojsonData();
+  // loadFourSquareData();
+
 
 main();
+function getRandomLatLng(map) {
+    // get the boundaries of the map
+    let bounds = map.getBounds();
+    let southWest = bounds.getSouthWest();
+    let northEast = bounds.getNorthEast();
+    let lngSpan = northEast.lng - southWest.lng;
+    let latSpan = northEast.lat - southWest.lat;
+
+    let randomLng = Math.random() * lngSpan + southWest.lng;
+    let randomLat = Math.random() * latSpan + southWest.lat;
+
+    return [ randomLat, randomLng,];
+}
+function getRandomLatLng(map) {
+    // get the boundaries of the map
+    let bounds = map.getBounds();
+    let southWest = bounds.getSouthWest();
+    let northEast = bounds.getNorthEast();
+    let lngSpan = northEast.lng - southWest.lng;
+    let latSpan = northEast.lat - southWest.lat;
+
+    let randomLng = Math.random() * lngSpan + southWest.lng;
+    let randomLat = Math.random() * latSpan + southWest.lat;
+
+    return [ randomLat, randomLng,];
+}
+
+// create marker cluster
+let markerClusterLayer = L.markerClusterGroup();
+
+for (let i = 0; i < 1000; i++) {
+    let pos = getRandomLatLng(map);
+    L.marker(pos).addTo(markerClusterLayer);
+}
+markerClusterLayer.addTo(map);
